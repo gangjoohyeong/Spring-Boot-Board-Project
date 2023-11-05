ea8625ad7e91172e7514cf877fcaebf5605a38b5
kangjoohyeong <bles@kakao.com>
2023-11-05 15:47:52 +0900

    feat: add Repository, Test code tutorial

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,13 +1,33 @@
 package com.mysite.sbb;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 class SbbApplicationTests {
 
+	@Autowired // Test Code에서는 @Autowired 사용, 실제로는 생성자를 통한 객체 주입 권장
+	private QuestionRepository questionRepository;
+
+	@Transactional // Test Code에서 DB 세션이 종료되지 않도록 방지
 	@Test
-	void contextLoads() {
+	void testJpa() {
+		Optional<Question> oq = this.questionRepository.findById(2);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+
+		List<Answer> answerList = q.getAnswerList();
+
+		assertEquals(1, answerList.size());
+		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
 	}
 
 }
