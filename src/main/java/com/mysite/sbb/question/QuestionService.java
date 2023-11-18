c25f6adab4254106b8efc77e8481b98f516461f2
gangjoohyeng <bles@kakao.com>
2023-11-18 14:59:47 +0900

    feat: add qna modify, delete

--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -28,21 +28,30 @@ public class QuestionService {
         return this.questionRepository.findAll(pageable);
     }
 
-        public Question getQuestion(Integer id) {
-            Optional<Question> question = this.questionRepository.findById(id);
-            if (question.isPresent()) {
-                return question.get();
-            } else {
-                throw new DataNotFoundException("question not found");
-            }
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
         }
+    }
 
-        public void create(String subject, String content, SiteUser user) {
-            Question q = new Question();
-            q.setSubject(subject);
-            q.setContent(content);
-            q.setCreateDate(LocalDateTime.now());
-            q.setAuthor(user);
-            this.questionRepository.save(q);
-        }
+    public void create(String subject, String content, SiteUser user) {
+        Question q = new Question();
+        q.setSubject(subject);
+        q.setContent(content);
+        q.setCreateDate(LocalDateTime.now());
+        q.setAuthor(user);
+        this.questionRepository.save(q);
+    }
+    public void modify(Question question, String subject, String content) {
+        question.setSubject(subject);
+        question.setContent(content);
+        question.setModifyDate(LocalDateTime.now());
+        this.questionRepository.save(question);
+    }
+    public void delete(Question question) {
+        this.questionRepository.delete(question);
+    }
 }
